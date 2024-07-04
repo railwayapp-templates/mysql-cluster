42b9b77c013e1e59075959c5434be621376b3169
Melissa Hale <melissakayh@gmail.com>
2024-07-03 22:11:39 -0500

    correctly set config data

---
+++ nodes/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Replace placeholders in the my.cnf file with actual environment variable values
+sed -i "s/@@SERVER_ID@@/$SERVER_ID/g" /etc/mysql/my.cnf
+sed -i "s/@@HOSTNAME@@/$HOSTNAME/g" /etc/mysql/my.cnf
+
+# Start MySQL server
+exec mysqld
