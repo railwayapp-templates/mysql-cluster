ec4bab3591c2ba5c4ff830f55379d20f7010bf1b
Melissa Hale <melissakayh@gmail.com>
2024-07-05 21:44:46 -0500

    update config and test migration

--- nodes/setup.sh
+++ nodes/setup.sh
@@ -4,5 +4,14 @@
 sed -i "s/@@SERVER_ID@@/$SERVER_ID/g" /etc/mysql/my.cnf
 sed -i "s/@@HOSTNAME@@/$HOSTNAME/g" /etc/mysql/my.cnf
 
+
+echo "Checking the data directory"
+
+# Ensure the data directory is initialized
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MySQL data directory..."
+    mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
+fi
+
 # Start MySQL server
 docker-entrypoint.sh mysqld
