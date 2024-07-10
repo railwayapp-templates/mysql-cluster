9a42e2f302a744e9ab3e148c90afeb8a8a368d64
Melissa Hale <melissakayh@gmail.com>
2024-07-10 18:59:24 -0500

    make cluster name variable

--- initService/initCluster.js
+++ initService/initCluster.js
@@ -2,12 +2,13 @@ var dbPassword = "@@MYSQL_ROOT_PASSWORD@@";
 var mysql1Host = "@@MYSQL1_HOST_NAME@@";
 var mysql2Host = "@@MYSQL2_HOST_NAME@@";
 var mysql3Host = "@@MYSQL3_HOST_NAME@@";
+var clusterName = "@@CLUSTER_NAME@@";
 
 dba.configureInstance({user: 'root', host: mysql1Host, password: dbPassword});
 dba.configureInstance({user: 'root', host: mysql2Host, password: dbPassword});
 dba.configureInstance({user: 'root', host: mysql3Host, password: dbPassword});
 
-var cluster = dba.createCluster('testCluster');
+var cluster = dba.createCluster(clusterName);
 cluster.addInstance({user: 'root', host: mysql2Host, password: dbPassword}, {recoveryMethod: 'clone'});
 cluster.addInstance({user: 'root', host: mysql3Host, password: dbPassword}, {recoveryMethod: 'clone'});
 print(cluster.status());
