d1ca222cc531dc201cf792b4deefc565923cb46e
Melissa Hale <melissakayh@gmail.com>
2024-08-15 15:58:16 -0600

    exit instead of delete init service
    Signed-off-by: Melissa Hale <melissakayh@gmail.com>

--- initService/setup.sh
+++ initService/setup.sh
@@ -42,20 +42,4 @@ mysqlsh root@"$MYSQL1_HOST_NAME":3306 --password=$MYSQL_ROOT_PASSWORD --file=/tm
 echo "Executing GraphQL mutation to restart the router service..."
 restart_service "$ENVIRONMENT_ID" "$ROUTER_SERVICE_ID"
 
-# Remove this service
-echo "Executing GraphQL mutation to remove the init service..."
-response=$(curl --silent --location "$RAILWAY_API_URL" \
-  --header 'Content-Type: application/json' \
-  --header "Authorization: Bearer $RAILWAY_API_TOKEN" \
-  --data "{\"query\":\"mutation serviceDelete(\$environmentId: String, \$id: String!) { serviceDelete(environmentId: \$environmentId, id: \$id) }\",\"variables\":{\"environmentId\":\"$ENVIRONMENT_ID\",\"id\":\"$SERVICE_ID\"}}")
-
-# Check for errors in the GraphQL response
-if echo "$response" | grep -q '"errors"'; then
-  echo "Failed to delete the service via the API. Response: $response"
-  exit 1
-else
-  echo "GraphQL mutation executed successfully. Response: $response"
-fi
-
-# Exit after setup is complete
 exit 0
